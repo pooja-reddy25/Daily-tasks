a1f014cbb3b87f7d7ce9b9a745e4ce19b4b1e894
pooja-reddy25 <poojavreddy25@gmail.com>
2024-11-21 13:55:08 +0530

    New Folder Created for Leet code Questions

---
+++ 21-11-2024/leetCodeQues.js
@@ -0,0 +1,8 @@
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+function createHelloWorld(){
+    return function(){
+        return "Hello World";
+    };
+}
+const hello = createHelloWorld();
+console.log(hello());
