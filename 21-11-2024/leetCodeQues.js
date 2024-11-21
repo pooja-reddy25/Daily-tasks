0415333a37032b4bd312b21d56521e7443f84714
pooja-reddy25 <poojavreddy25@gmail.com>
2024-11-21 14:02:31 +0530

    New program added for Leet code folder

--- 21-11-2024/leetCodeQues.js
+++ 21-11-2024/leetCodeQues.js
@@ -6,3 +6,16 @@ function createHelloWorld(){
 }
 const hello = createHelloWorld();
 console.log(hello());
+
+
+// Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+function createCounter(n){
+    let counter = n;
+    return function(){
+        return counter++;
+    };
+}
+const count = createCounter(5);
+console.log(count());
+console.log(count());
+console.log(count());
