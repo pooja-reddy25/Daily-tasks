ee6d8c3b131a307e69aa3024f246669ce228ed33
pooja-reddy25 <poojavreddy25@gmail.com>
2024-11-22 15:52:12 +0530

    Practice programs involving functions and strings.

--- 22-11-2024/functions.js
+++ 22-11-2024/functions.js
@@ -19,19 +19,68 @@ function log(){
 }
 log();
 
+
+// Functions along with string methods
+// LENGTH
 function length(string){
     return string.length;
     }
 console.log(length('sun'));
 
+//toLowerCase and toUpperCase
 function toCase(string){
     return string.toLowerCase()+'-'+string.toUpperCase();
     }
 console.log(toCase('Mthatha'));
 
+//charAt
 function shortcut(str1,str2){
     return str1.charAt(0) + str2.charAt(0);
     }
 let result = shortcut('Amnesty','International');
 console.log(result);
-    
+
+//TRIM
+function firstChar(string){
+    string=string.trim();
+    return string.charAt(0);
+    }
+    let string = firstChar(' Rosa Parks ');
+    console.log(string);
+
+//INDEXOF
+function indexOfIgnoreCase(string1,string2){
+    return string1.toLowerCase().indexOf(string2.toLowerCase());
+    }
+console.log(indexOfIgnoreCase('bit','it'));
+console.log(indexOfIgnoreCase('bit','IT'));
+
+
+function secondIndexOf(s1,s2){
+    let firstIndex = s1.indexOf(s2);
+    return s1.indexOf(s2,firstIndex+1);
+    }
+    let result1 = secondIndexOf('White Rabbit', 'it');
+    console.log(result1); 
+
+//SUBSTR
+function firstWord(s){
+    let firstBlank = s.indexOf(' ');
+    return s.substr(0,firstBlank);
+}
+console.log(firstWord('see and stop'));
+console.log(firstWord('speak again'));
+
+//REPLACE AND REPLACEALL
+function normalize(string){
+    let result2 = string.replaceAll('-','/');
+    return result2;
+    }
+console.log(normalize('25-4-2002'));
+//or
+function normalize1(date){
+    let newDate = date.replace('-','/');
+    newDate = newDate.replace('-','/')
+    return newDate;
+}
+console.log(normalize1('15-1-1997'));
